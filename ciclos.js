deb859968fe929eca771c7fb705c40bf4931b426
shavaxzz <lc16892@escueladeciencias.edu.mx>
2023-06-09 23:06:55 -0600

    primer

---
+++ ciclos.js
@@ -0,0 +1,10 @@
+function ciclo() {
+    let numero = 1;
+    for (let numerod = 1; numero <= 56; numerod++) {
+    console.log(numero);   
+    numero += numerod;
+    }
+    }
+    ciclo()
+    
+    
